d6043d16e0dfa83cfbf3f1cc87ccbfad65f127a1
Hiroki Kato <hirokikato1@gmail.com>
2023-07-30 20:34:56 -0400

    update components demonstrating JSX principles

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,10 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  const colorFireBrick = {color :'firebrick'}
+  return (<div id="home">
+    <h1 style={colorFireBrick}>{name} is a Web Developer from {city}</h1>
+  </div>);
 }
 
 export default Home;
